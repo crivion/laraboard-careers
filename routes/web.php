919766994454ac9abdd07dc6435faaef726eccdc
Alex Crivion <crivion@gmail.com>
2023-04-24 20:00:03 +0300

    cleanup of unused controllers

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Front\HomepageController;
 use App\Http\Controllers\Front\JobDetailsController;
 use App\Http\Controllers\Front\StoreJobApplicationController;
-use App\Http\Controllers\Front\TeamController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +20,6 @@ Route::any('/', HomepageController::class)->name('homepage');
 Route::get('position/{job}', JobDetailsController::class)->name('jobDetails');
 Route::post('position/{job}/store-application', [StoreJobApplicationController::class, 'store'])->name('storeJobApplication');
 Route::get('application/{job}', [StoreJobApplicationController::class, 'applicationReceived'])->name('jobApplicationReceived');
-Route::get('team', TeamController::class)->name('team');
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
